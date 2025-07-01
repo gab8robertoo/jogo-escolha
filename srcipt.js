469d1db9de0cb8c2df3cc1d05a295310509a3ec5
gab8robertoo <145361926+gab8robertoo@users.noreply.github.com>
2025-07-01 11:36:34 -0300

    .

---
+++ srcipt.js
@@ -0,0 +1,31 @@
+const caixaPincipal = document.querySelector(".caixa-principal");
+const caixaPincipal = document.querySelector(".caixa-perguntas");
+const caixaPincipal = document.querySelector(".caixa-alternativas");
+const caixaPincipal = document.querySelector(".caixa-resultado");
+const textoPincipal = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: Hoje é um dia f ew csdcsdlsdnsjvnskjvnsjds,
+        arternativas:[
+        "isso e bla",
+        "blaslslskl",
+        ]
+
+    },
+    {
+        enunciado: assimdkmvknvksfnvksnvksnkfsn,
+           alternativas:[
+            "onfjnjnffsg,mf"
+            "fdmkvdfmkvdfm"
+          
+        ]
+
+    },
+    {
+       enunciado: assimdkmvknvksfnvksnvksnkfsn,
+           alternativas:[
+            "onfjnjnffsg,mf"
+            "fdmkvdfmkvdfm" 
+    }
+]
